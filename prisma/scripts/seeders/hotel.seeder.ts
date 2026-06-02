260c4b3d3b66425b9c35d36e829d74174c0251ae
Rafayel <jlavyan2021@gmail.com>
2026-06-02 19:06:03 +0400

    fix hotels image

--- prisma/scripts/seeders/hotel.seeder.ts
+++ prisma/scripts/seeders/hotel.seeder.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 export async function seedHotels(prisma: PrismaClient) {
   console.log('🏨 Seeding 50 hotels...');
 
-  const hotelsData = Array.from({ length: 50 }).map(() => {
+  const hotelsData = Array.from({ length: 50 }).map((_, index) => {
     const hotelName = faker.company.name() + ' Hotel & Spa';
     const city = faker.location.city();
     const country = faker.location.country();
@@ -18,11 +18,7 @@ export async function seedHotels(prisma: PrismaClient) {
       pricePerNight: parseFloat(
         faker.commerce.price({ min: 50, max: 450, dec: 2 }),
       ),
-      image: faker.image.urlLoremFlickr({
-        category: 'hotel,room',
-        width: 640,
-        height: 480,
-      }),
+      image: `https://picsum.photos/640/480?random=${index + 1}`,
     };
   });
 
